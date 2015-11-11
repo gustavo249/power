f0e850bd504e61aacdb0983de608d993ed728ce3
Raoul CRISAN <rcrisan@e1p74.cluj.42.fr>
2015-11-11 12:49:50 +0100

    ds

---
+++ teme/problema2.c
@@ -0,0 +1,88 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   problema2.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rcrisan <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/11 11:53:10 by rcrisan           #+#    #+#             */
+/*   Updated: 2015/11/11 12:49:41 by rcrisan          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int		cmmdc(unsigned int a, unsigned int b)
+{
+	int r;
+
+	r = 0;
+	while (b)
+	{
+		r = a % b;
+		a = b;
+		b = r;
+	}
+	return (a);
+}
+
+int	suma_cifre(int n)
+{
+	int sum;
+
+	sum = 0;
+	while (n != 0)
+	{
+		sum = sum + n % 10;
+		n = n / 10;
+	}
+	return (sum);
+}
+
+int		is_prime(int n)
+{
+	int c;
+
+	c = 2;
+	while (c * c <= n)
+	{
+		if (n % c == 0)
+			return (0);
+		c++;
+	}
+	return (1);
+}
+int main ()
+{
+	unsigned int n[6]; //= { 62, 8, 41, 23, 64, 36, 12};
+	int i = 0;
+	int size = sizeof(n) / sizeof(n[i]);
+	while (i < size)
+	{
+		printf("Introdu al %d -lea vectorului:\n", i);
+		scanf("%d", &n[i]);
+		i++;
+	}
+	i = 0;
+	while (i < size - 1)
+	{
+		printf("\nCMMDC a lui %d si %d este : %d\n",n[i], n[i + 1], cmmdc(n[i], n[i + 1]));
+		i++;
+	}
+	i = 0;
+	while (i < size - 1)
+	{
+		if (suma_cifre(n[i]) == suma_cifre(n[i + 1]))
+			printf("\nsuma cifrelor nr | %d | este egala cu suma cifrelor nr | %d |, aceasta fiind egala cu %d\n"\
+				, n[i], n[i + 1], suma_cifre(n[i]));
+		i++;
+	}
+	i = 0;
+	while (i < size - 1)
+	{
+		if (is_prime(n[i]) == 1 && is_prime(n[i + 1]) == 1)
+			printf("Perechea aceasta de numere contine numere prime: |%d|, |%d|\n", n[i], n[i + 1]);
+		i++;
+	}
+	return (0);
+}
