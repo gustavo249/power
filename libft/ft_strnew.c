35f540b19500182abf927af0cdf55d59cb76e6d4
Raoul CRISAN <rcrisan@e1p74.cluj.42.fr>
2015-11-16 15:47:51 +0100

    ds

--- libft/ft_strnew.c
+++ libft/ft_strnew.c
@@ -6,7 +6,7 @@
 /*   By: rcrisan <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/31 03:57:14 by rcrisan           #+#    #+#             */
-/*   Updated: 2015/11/06 16:22:12 by rcrisan          ###   ########.fr       */
+/*   Updated: 2015/11/16 15:47:43 by rcrisan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,17 @@
 
 char	*ft_strnew(size_t size)
 {
-	void	*str;
+	char	*s;
+	size_t	i;
 
-	if (size == 1)
-		return ((char*)NULL);
-	str = (void*)malloc(size + 1);
-	return ((char*)str);
+	i = 0;	
+	s = (char*)malloc(sizeof(*s) * size);
+	if (s == NULL)
+		return (NULL);
+	while (i <= size)
+	{
+		s[i] = '\0';
+		i++;
+	}
+	return (s);
 }
