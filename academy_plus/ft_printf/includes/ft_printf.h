a7ad53d5f471cc4d4968b877e063de18c1c5812d
Raoul CRISAN <rcrisan@e1p19.cluj.42.fr>
2016-01-05 16:35:41 +0100

    ds

--- academy_plus/ft_printf/includes/ft_printf.h
+++ academy_plus/ft_printf/includes/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: rcrisan <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/04 19:52:30 by rcrisan           #+#    #+#             */
-/*   Updated: 2016/01/05 14:59:19 by rcrisan          ###   ########.fr       */
+/*   Updated: 2016/01/05 16:16:54 by rcrisan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,25 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include "../libft/libft.h"
+
+typedef struct	mod
+{
+	int		h_mod;
+	int		hh_mod;
+	int		l_mod;
+	int		ll_mod;
+	int		z_mod;
+	int		j_mod;
+	int		hash_mod;
+	int		minus_mod;
+	int		zero_mod;
+	int		plus_mod;
+	int		space_mod;
+	char	flag;
+	int		width;
+	int		precision;
+
+}				t_mod;
 
 int		get_width(char *format);
 
