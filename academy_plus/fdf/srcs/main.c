099add5522acb1024bb744ec7026f276ec931fb6
Raoul CRISAN <rcrisan@e1p24.cluj.42.fr>
2016-01-30 14:48:20 +0100

    ds

--- academy_plus/fdf/srcs/main.c
+++ academy_plus/fdf/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: rcrisan <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/28 13:00:38 by rcrisan           #+#    #+#             */
-/*   Updated: 2016/01/29 19:27:52 by rcrisan          ###   ########.fr       */
+/*   Updated: 2016/01/30 14:48:02 by rcrisan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,7 @@ void	init_struct(t_mod *data)
 	data->error = 0;
 }
 
-int		validate_content(char	*content)
-{
-	int i;
-
-	i = 0;
-	while (content[i])
-	{
-		if (!ft_isdigit(content[i]) && content[i] != '-')
-			return (-1);
-		i++;
-	}
-	return (1);
-}
-
-int		**add_new_content(int **new_matrix, char **row, t_mod *data, int *i)
+int		**add_new_content(int **new_matrix, t_mod *data, int *i)
 {
 	int j;
 	int k;
@@ -43,16 +29,16 @@ int		**add_new_content(int **new_matrix, char **row, t_mod *data, int *i)
 	new_matrix[p] = (int*)malloc(sizeof(int) * data->cols);
 	k = 0;
 	j = 0;
-	while (row[k])
+	while (data->content[k])
 	{
-		new_matrix[p][j] = ft_atoi(row[k]);
+		new_matrix[p][j] = ft_atoi(data->content[k]);
 		k++;
 		j++;
 	}
 	return (new_matrix);
 }
 
-int		**add_new_row(int **matrix, char **row, t_mod *data)
+int		**add_new_row(t_mod *data)
 {
 	int		**new_matrix;
 	int		i;
@@ -67,28 +53,28 @@ int		**add_new_row(int **matrix, char **row, t_mod *data)
 		j = 0;
 		while (j < data->cols)
 		{
-			new_matrix[i][j] = matrix[i][j];	
+			new_matrix[i][j] = data->matrix[i][j];	
 			j++;
 		}
 		i++;
 	}
-	new_matrix = add_new_content(new_matrix, row, data, &i);
+	new_matrix = add_new_content(new_matrix, data, &i);
 	return (new_matrix);
 }
 
-void	print_matrix(int	**matrix, t_mod *data)
+void	print_matrix(t_mod *data)
 {
 	int i, j;
 
 	for (i = 0; i < data->rows; i++)
 	{
 		for (j = 0; j < data->cols; j++)
-			printf(" %d", matrix[i][j]);
+			printf(" %d", data->matrix[i][j]);
 		printf("\n");
 	}
 }
 
-int		**add_first_row(t_mod *data, char **first_row, int fd, int **matrix)
+int		**add_first_row(t_mod *data, int fd)
 {
 	char	*line;
 
@@ -96,46 +82,68 @@ int		**add_first_row(t_mod *data, char **first_row, int fd, int **matrix)
 
 	if (line != NULL)
 	{
-		first_row = ft_strsplit(line, ' ');	
-		while (first_row[data->cols] != '\0')
+		data->content = ft_strsplit(line, ' ');	
+		while (data->content[data->cols] != '\0')
 			data->cols++;
 		data->rows++;
-
-		matrix = add_new_row(matrix, first_row, data);
+		data->matrix = add_new_row(data);
 	}
 	else
 		data->error = 1;
-	return (matrix);
+	return (data->matrix);
 
 }
 
 int		read_matrix(int fd, t_mod *data)
 {
 	char	*line;
-	char	**first_row;
 	int		i;
 	int		cols;
-	int		**matrix;
 
-	matrix = NULL;
+	data->matrix = NULL;
 	i = 0;
 	cols = 0;
-	matrix = add_first_row(data, first_row, fd, matrix);
+	data->matrix = add_first_row(data, fd);
 	if (data->error == 1)
 		return (-1);
 	while (get_next_line(fd, &line))
 	{
 		cols = 0;
-		first_row = ft_strsplit(line, ' ');
-		while (first_row[cols] != '\0')
+		data->content = ft_strsplit(line, ' ');
+		while (data->content[cols] != '\0')
 			cols++;
 		if (cols != data->cols)
 			return (-1);
 		data->rows++;
-		matrix = add_new_row(matrix ,first_row, data);
+		data->matrix = add_new_row(data);
 	}
-	print_matrix(matrix, data);
+	print_matrix(data);
 	return (1);
+}
+
+void	draw_line(t_mod *data)
+{
+	int x;
+	int y;
+
+	y = 200;
+	while (y < 200)
+	{
+		x = 100;
+		while (x < 200)
+		{
+			mlx_pixel_put(data->mlx, data->win, x, y, 0xFF0000);
+			x++;
+		}
+		y++;
+	}
+}
+
+void	draw_map(t_mod *data)
+{
+	mlx_expose_hook(data->win, draw_line, data);
+	mlx_loop(data->mlx);
+	sleep(10);
 }
 
 int main (int argc, char **argv)
@@ -148,5 +156,6 @@ int main (int argc, char **argv)
 	fd = open (argv[1], O_RDONLY);
 	init_struct(&data);
 	read_matrix(fd, &data);
+	draw_map(&data);
 	close(fd);
 }
